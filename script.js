70afb2998fbf19e97b8a37597085b88e041ae1d0
Gabriel Henrique <137806276+BMBielzin@users.noreply.github.com>
2025-05-27 11:18:21 -0300

    ge

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ const perguntas = [
     alternativas: ["Queda da Bastilha", "Coronamento de Napoleão Bonaparte"],
   },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+  }
+}
