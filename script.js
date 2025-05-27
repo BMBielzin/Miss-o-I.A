e0966a9d59e01696945489ea2ac9d4a7e66cbb59
Gabriel Henrique <137806276+BMBielzin@users.noreply.github.com>
2025-05-27 11:32:35 -0300

    gh

--- script.js
+++ script.js
@@ -29,18 +29,24 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
   mostraAlternativas();
 }
-function mostraAlternativas() {}
 
-mostraPergunta();
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
   }
+function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal += afirmacoes + " ";
+  atual++;
+  mostraPergunta();
+ }
 }
+  mostraPergunta();
