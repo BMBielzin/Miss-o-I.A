575fac71737148c3e75c2b51da4cc87d00295fd1
Gabriel Henrique <137806276+BMBielzin@users.noreply.github.com>
2025-06-03 11:28:50 -0300

    fn

--- script.js
+++ script.js
@@ -5,27 +5,72 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-  {
-    enunciado: "Qual evento marcou o início da Nova Ordem Mundial, caracterizada por uma estrutura geopolítica multipolar?",
-    alternativas: ["Fim da Guerra Fria", "AAssinatura do Tratado de Versalhes"],
-  },
-  {
-    enunciado: "Qual dos seguintes países do Oriente Médio não faz parte da Organização dos Países Exportadores de Petróleo (OPEP)?",
-    alternativas: ["Irã", "Israel"],
-  },
-  {
-    enunciado: "Qual é o principal fator responsável pelo rápido envelhecimento da população brasileira nas últimas décadas?",
-    alternativas: ["Aumento da expectativa de vida e redução da taxa de fecundidade", "Migração em massa de idosos para áreas urbanas"],
-  },
-  {
-    enunciado: "Qual país é conhecido por sua política de neutralidade e por sediar importantes organizações internacionais, como a Cruz Vermelha e partes da ONU?",
-    alternativas: ["Suíça", "Alemanha"],
-  },
-  {
-    enunciado: "Qual evento histórico marcou o início da Revolução Francesa em 1789?",
-    alternativas: ["Queda da Bastilha", "Coronamento de Napoleão Bonaparte"],
-  },
-];
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+]
 
 let atual = 0;
 let perguntaAtual;
